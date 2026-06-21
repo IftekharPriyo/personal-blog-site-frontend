604b71d8fb6e6af79f3552aec15a2d5a2607a19c
Iftekhar Priyo <priyo.mobin123@gmail.com>
2026-06-21 21:28:28 +0600

    fixed: ngrok deployment error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const ngrokDevOrigin = process.env.NGROK_DEV_ORIGIN
+  ? new URL(process.env.NGROK_DEV_ORIGIN).hostname
+  : undefined;
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ngrokDevOrigin ? [ngrokDevOrigin] : [],
 };
 
 export default nextConfig;
